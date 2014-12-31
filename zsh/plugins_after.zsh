bc35010954379dfde743b1765c5a3daab76eea5f
Josh Price <josh@outlawit.com>
2014-12-31 16:13:49 -0500

    cleaning up

--- zsh/plugins_after.zsh
+++
@@ -1,6 +0,0 @@
-# External plugins (initialized after)
-
-# dircolors
-if [[ "$(tput colors)" == "256" ]]; then
-    eval $(dircolors ~/.zsh/plugins/dircolors-solarized/dircolors.256dark)
-fi
--- zsh/plugins_before.zsh
+++
@@ -1,4 +0,0 @@
-# External plugins (initialized before)
-
-# zsh-completions
-fpath=(~/.zsh/plugins/zsh-completions/src $fpath)
